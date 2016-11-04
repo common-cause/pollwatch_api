f74b25ff96b3e5579861f1f2f65528b49c56c06b
common-cause <rkerth@commoncause.org>
2016-11-04 15:31:38 -0400

    centralize all lookups in one API

--- ny_api.py
+++ ny_api.py
@@ -2,30 +2,58 @@ from flask import Flask, request
 from flask_restful import Api, Resource
 import psycopg2
 from local_settings import pw
+import requests
+import json
 
 conn = psycopg2.connect(host='pollwatchny.cweqetvzooza.us-west-2.rds.amazonaws.com',dbname='pollwatch',user='pollwatch',password=pw)
 
 app = Flask(__name__)
 api = Api(app)
 
+class AddressError(Exception):
+	def __init__(self):
+		pass
+
+def nyc_lookup(streetno, streetname, zipcode):
+	endpoint = 'https://nyc.electionapi.com/psl/pollsiteinfo'
+	payload = {'key' : '979e41df-0bf3-4ae4-97d7-16da76f6af65', 'streetnumber' : streetno, 'streetname' : streetname, 'postalcode' : zipcode}
+	api_data = json.loads(requests.get(endpoint,params=payload).text)
+	try:
+		ed_data = api_data['election_district'].split('/')
+		return {'assembly_district' : ed_data[0], 'election_district' : ed_data[1]}
+	except KeyError:
+		raise AddressError
+
+def upstate_lookup(streetno, streetname, zipcode):
+	curs = conn.cursor()
+	curs.execute("SELECT ad, ed FROM nyfile WHERE streetno = '%s' AND (streetname = '%s' OR streetname = replace('%s','TRAIL','TRL')) AND zip = '%s'" % (streetno.upper(), streetname.upper(), streetname.upper(), zipcode))
+	conn.commit()
+	data = curs.fetchall()
+	if len(data) == 0:
+		raise AddressError
+	else:
+		del curs
+		return {'assembly_district' : data[0][0], 'election_district' : data[0][1]}
+
 class Address(Resource):
 	def get(self):
 		streetno = request.args.get('streetno')
 		streetname = request.args.get('streetname')
 		zipcode = request.args.get('zipcode')
-		curs = conn.cursor()
-		curs.execute("SELECT ad, ed FROM nyfile WHERE streetno = '%s' AND (streetname = '%s' OR streetname = replace('%s','TRAIL','TRL')) AND zip = '%s'" % (streetno.upper(), streetname.upper(), streetname.upper(), zipcode))
-		conn.commit()
-		data = curs.fetchall()
-		if len(data) == 0:
+		county = request.args.get('county')
+		try:
+			if county in ['Kings County','New York County','Richmond County','Queens County','Bronx County']:
+				return nyc_lookup(streetno, streetname, zipcode)
+			else:
+				return upstate_lookup(streetno, streetname, zipcode)
+		except AddressError:
 			return {'Error' : 'AddressNotFound','streetno' : streetno, 'streetname': streetname, 'zipcode' : zipcode}
-		else:
-			return {'assembly_district' : data[0][0], 'election_district' : data[0][1]}
-		del curs
-		
+	
 		
 api.add_resource(Address,'/')
 
 
 if __name__ == '__main__':
         app.run()
+
+		
